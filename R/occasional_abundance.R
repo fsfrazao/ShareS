32a3739b1e03a33609c32091a2d5f7d9d8bc1cf6
Fabio Frazao <fsfrazao@gmail.com>
2019-06-15 21:32:48 -0300

    Add sample data

--- R/occasional_abundance.R
+++ R/occasional_abundance.R
@@ -1,3 +1,12 @@
+#' Converts Fahrenheit to Kelvin
+#'
+#' This function converts input temperatures in Fahrenheit to Kelvin.
+#' @param temp_F The temperature in Fahrenheit.
+#' @return The temperature in Kelvin.
+#' @export
+#' @examples
+#' fahrenheit_to_kelvin(32)
+
 occasional_abundance<-function(data,abund_class,dont_apply_to=0){
 	
 	res_mat<- apply(data,2,function(x) if (sum(x)>abund_class) result<-x else result<-rep(0,length(x)))
