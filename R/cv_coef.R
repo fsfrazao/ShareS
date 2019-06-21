d880af5e381d841de7791939abfb6b693b461bda
Fabio Frazao <fsfrazao@gmail.com>
2019-06-21 16:57:07 -0300

    cv_coef-> Update documentation

--- R/cv_coef.R
+++ R/cv_coef.R
@@ -1,11 +1,28 @@
-#' Converts Fahrenheit to Kelvin
+#' Extracts conservation value coefficients from a pair a community matr conservation value metricsices
 #'
-#' This function converts input temperatures in Fahrenheit to Kelvin.
-#' @param temp_F The temperature in Fahrenheit.
-#' @return The temperature in Kelvin.
+#' This function takes two community matrices and extracts 3 coefficients used to calculate conservation value metrics
+#' @param x A community matrix, with species in columns and samples in rows
+#' @param y A community matrix, with species in columns and samples in rows
+#' @return a list with $shared (the number of shared species), $unique_x (the number of species unique to community x) and 
+#' $unique_y (the number of species unique to community y)
 #' @export
 #' @examples
-#' fahrenheit_to_kelvin(32)
+#' data(community_data)
+#'
+#' #Select all samples in Evironment 'A'
+#' comm.A<-community_data[which(community_data$environment == "A"),]
+#'
+#' # Select only the abundance matrix (columns 1 and 2 contain the 'environment' and 'sample' data)
+#' comm.A <- comm.A[, 3:ncol(comm.A)]
+#'
+#' #Select all samples in Evironment 'B'
+#' comm.B<-community_data[which(community_data$environment == "B"),]
+#'
+#' # Select only the abundance matrix (columns 1 and 2 contain the 'environment' and 'sample' data)
+#' comm.B <- comm.B[, 3:ncol(comm.B)]
+#'
+#' #Calculate coefficients
+#' cv_coef(comm.A, comm.B)
 
 cv_coef<-function(x,y){
 
