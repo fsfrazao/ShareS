c19aa58b3592d46382de402f584faae3f30f552e
Fabio Frazao <fsfrazao@gmail.com>
2019-06-22 17:09:23 -0300

    Conservation_value_seq_CI->Substitute '.' for '_' in functions and variable names

--- R/conservation_value_seq_CI.R
+++ R/conservation_value_seq_CI.R
@@ -1,6 +1,9 @@
-conservation.value.seq.CI<-function(x.boot,y.boot,seq,removal.rule=2,occasional="a"){
+\
 
-if (dim(x.boot)[3]!=dim(y.boot)[3]) stop("x.boot and y.boot must have the same number of randomizations") else rand=dim(x.boot)[3]
+
+conservation_value_seq_CI<-function(x_boot,y_boot,seq,removal_rule=2,occasional="a"){
+
+if (dim(x_boot)[3]!=dim(y_boot)[3]) stop("x_boot and y_boot must have the same number of randomizations") else rand=dim(x_boot)[3]
 
 results<-array(dim=c(seq+1,3,3))
 
@@ -15,59 +18,59 @@ m3<-vector(length=rand)
 
 	for (i in 1:(seq+1)){
 			
-		if (occasional=="a" & removal.rule==2){		
+		if (occasional=="a" & removal_rule==2){		
 			
 			for (j in 1:rand){
-				a[j]<-cv.coef(occasional.abundance(x.boot[,,j],i-1),occasional.abundance(y.boot[,,j],i-1))$shared
-				b[j]<-cv.coef(occasional.abundance(x.boot[,,j],i-1),occasional.abundance(y.boot[,,j],i-1))$unique.x
-				c[j]<-cv.coef(occasional.abundance(x.boot[,,j],i-1),occasional.abundance(y.boot[,,j],i-1))$unique.y
+				a[j]<-cv_coef(occasional_abundance(x_boot[,,j],i-1),occasional_abundance(y_boot[,,j],i-1))$shared
+				b[j]<-cv_coef(occasional_abundance(x_boot[,,j],i-1),occasional_abundance(y_boot[,,j],i-1))$unique_x
+				c[j]<-cv_coef(occasional_abundance(x_boot[,,j],i-1),occasional_abundance(y_boot[,,j],i-1))$unique_y
 			}
 		}
 
-		if (occasional=="a" & removal.rule==1){		
+		if (occasional=="a" & removal_rule==1){		
 			
 			for (j in 1:rand){
-				a[j]<-cv.coef(x.boot[,,j],occasional.abundance(y.boot[,,j],i-1))$shared
-				b[j]<-cv.coef(x.boot[,,j],occasional.abundance(y.boot[,,j],i-1))$unique.x
-				c[j]<-cv.coef(x.boot[,,j],occasional.abundance(y.boot[,,j],i-1))$unique.y
+				a[j]<-cv_coef(x_boot[,,j],occasional_abundance(y_boot[,,j],i-1))$shared
+				b[j]<-cv_coef(x_boot[,,j],occasional_abundance(y_boot[,,j],i-1))$unique_x
+				c[j]<-cv_coef(x_boot[,,j],occasional_abundance(y_boot[,,j],i-1))$unique_y
 			}
 		}	
 
-		if (occasional=="o" & removal.rule==2 & dim(x.boot)[1]>=(i-1)){		
+		if (occasional=="o" & removal_rule==2 & dim(x_boot)[1]>=(i-1)){		
 			
 			for (j in 1:rand){
-				a[j]<-cv.coef(occasional.occupancy(x.boot[,,j],i-1,0),occasional.occupancy(y.boot[,,j],i-1,0))$shared
-				b[j]<-cv.coef(occasional.occupancy(x.boot[,,j],i-1,0),occasional.occupancy(y.boot[,,j],i-1,0))$unique.x
-				c[j]<-cv.coef(occasional.occupancy(x.boot[,,j],i-1,0),occasional.occupancy(y.boot[,,j],i-1,0))$unique.y
+				a[j]<-cv_coef(occasional_occupancy(x_boot[,,j],i-1,0),occasional_occupancy(y_boot[,,j],i-1,0))$shared
+				b[j]<-cv_coef(occasional_occupancy(x_boot[,,j],i-1,0),occasional_occupancy(y_boot[,,j],i-1,0))$unique_x
+				c[j]<-cv_coef(occasional_occupancy(x_boot[,,j],i-1,0),occasional_occupancy(y_boot[,,j],i-1,0))$unique_y
 			}
 			
 		}			
 
-		if (occasional=="o" & removal.rule==1){		
+		if (occasional=="o" & removal_rule==1){		
 			
 			for (j in 1:rand){
-				a[j]<-cv.coef(x.boot[,,j],occasional.occupancy(y.boot[,,j],i-1,0))$shared
-				b[j]<-cv.coef(x.boot[,,j],occasional.occupancy(y.boot[,,j],i-1,0))$unique.x
-				c[j]<-cv.coef(x.boot[,,j],occasional.occupancy(y.boot[,,j],i-1,0))$unique.y
+				a[j]<-cv_coef(x_boot[,,j],occasional_occupancy(y_boot[,,j],i-1,0))$shared
+				b[j]<-cv_coef(x_boot[,,j],occasional_occupancy(y_boot[,,j],i-1,0))$unique_x
+				c[j]<-cv_coef(x_boot[,,j],occasional_occupancy(y_boot[,,j],i-1,0))$unique_y
 			}
 		}			
 		
-		if (occasional=="b" & removal.rule==2 & dim(x.boot)[1]>=(i-1)){		
+		if (occasional=="b" & removal_rule==2 & dim(x_boot)[1]>=(i-1)){		
 			
 			for (j in 1:rand){
-				a[j]<-cv.coef(occasional.occupancy(x.boot[,,j],i-1,i-1),occasional.occupancy(y.boot[,,j],i-1,i-1))$shared
-				b[j]<-cv.coef(occasional.occupancy(x.boot[,,j],i-1,i-1),occasional.occupancy(y.boot[,,j],i-1,i-1))$unique.x
-				c[j]<-cv.coef(occasional.occupancy(x.boot[,,j],i-1,i-1),occasional.occupancy(y.boot[,,j],i-1,i-1))$unique.y
+				a[j]<-cv_coef(occasional_occupancy(x_boot[,,j],i-1,i-1),occasional_occupancy(y_boot[,,j],i-1,i-1))$shared
+				b[j]<-cv_coef(occasional_occupancy(x_boot[,,j],i-1,i-1),occasional_occupancy(y_boot[,,j],i-1,i-1))$unique_x
+				c[j]<-cv_coef(occasional_occupancy(x_boot[,,j],i-1,i-1),occasional_occupancy(y_boot[,,j],i-1,i-1))$unique_y
 			}
 			
 		}			
 		
-		if (occasional=="b" & removal.rule==1){		
+		if (occasional=="b" & removal_rule==1){		
 			
 			for (j in 1:rand){
-				a[j]<-cv.coef(x.boot[,,j],occasional.occupancy(y.boot[,,j],i-1,i-1))$shared
-				b[j]<-cv.coef(x.boot[,,j],occasional.occupancy(y.boot[,,j],i-1,i-1))$unique.x
-				c[j]<-cv.coef(x.boot[,,j],occasional.occupancy(y.boot[,,j],i-1,i-1))$unique.y
+				a[j]<-cv_coef(x_boot[,,j],occasional_occupancy(y_boot[,,j],i-1,i-1))$shared
+				b[j]<-cv_coef(x_boot[,,j],occasional_occupancy(y_boot[,,j],i-1,i-1))$unique_x
+				c[j]<-cv_coef(x_boot[,,j],occasional_occupancy(y_boot[,,j],i-1,i-1))$unique_y
 			}
 		}
 
@@ -75,9 +78,9 @@ m3<-vector(length=rand)
 
 
 			for(k in 1:rand){
-				m1[k]<-conservation.value(a[k],b[k],c[k])$m1
-				m2[k]<-conservation.value(a[k],b[k],c[k])$m2
-				m3[k]<-conservation.value(a[k],b[k],c[k])$m3
+				m1[k]<-conservation_value(a[k],b[k],c[k])$m1
+				m2[k]<-conservation_value(a[k],b[k],c[k])$m2
+				m3[k]<-conservation_value(a[k],b[k],c[k])$m3
 			}
 				
 		
@@ -99,9 +102,10 @@ m3<-vector(length=rand)
  }
 	colnames(results)<-c("m","2.5%","97.5%")
 	rownames(results)<-0:seq
-	results.list<-list(m1=results[,,1],m2=results[,,2],m3=results[,,3])
+	results_list<-list(m1=results[,,1],m2=results[,,2],m3=results[,,3])
 
-	return(results.list)
+	return(results_list)
 	
 }
 		
+       
